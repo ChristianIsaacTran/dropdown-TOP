d591fb49e6c6a4b90e6891e80fa75278b9813df4
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-05-15 02:43:16 -0500

    published but doesn't functionally work
    Learned how to publish to npm, but not how to make
    modules to use from npm packages.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import "./style/style.css";
 import dropdown from "./modules/dropdown.js";
 
-//Query the hardcoded dropdown button and options
-// const dropButton1 = document.querySelector(".dropdown-toggle.drop1");
-// const dropContent1 = document.querySelector(".dropdown-content.drop1");
-// const dropButton2 = document.querySelector(".dropdown-toggle.drop2");
-// const dropContent2 = document.querySelector(".dropdown-content.drop2");
+// Query the hardcoded dropdown button and options
+const dropButton1 = document.querySelector(".dropdown-toggle.drop1");
+const dropContent1 = document.querySelector(".dropdown-content.drop1");
+const dropButton2 = document.querySelector(".dropdown-toggle.drop2");
+const dropContent2 = document.querySelector(".dropdown-content.drop2");
 
-// //Add dropdown logic to both dropdowns
-// const dropdown1 = dropdown(dropButton1, dropContent1);
-// const dropdown2 = dropdown(dropButton2, dropContent2);
+//Add dropdown logic to both dropdowns
+const dropdown1 = dropdown(dropButton1, dropContent1);
+const dropdown2 = dropdown(dropButton2, dropContent2);
 
-// dropdown1.createDropDownLogic();
-// dropdown2.createDropDownLogic();
+dropdown1.createDropDownLogic();
+dropdown2.createDropDownLogic();
 
-module.exports = dropdown;
+// module.exports = dropdown;
